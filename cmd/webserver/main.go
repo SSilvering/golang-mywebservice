221e3680a2cbda84ab3040b786326d8a6f01976c
Shai Hod <silvering@gmail.com>
2022-02-26 18:18:47 +0200

    first implementaion of web server

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.Router)
+	log.Println("Listening...")
+	err := http.ListenAndServe(":3000", mux)
 
-	http.HandleFunc("/", handlers.Router)
+	// http.HandleFunc("/", handlers.Router)
 
-	err := http.ListenAndServe(":3000", nil)
+	// err := http.ListenAndServe(":3000", nil)
 
 	if err != nil {
 		log.Fatal(err)
