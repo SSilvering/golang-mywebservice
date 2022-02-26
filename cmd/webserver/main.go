d5a9f1b381a04afa12b504cf904ddb97916c172d
Shai Hod <silvering@gmail.com>
2022-02-26 18:17:00 +0200

    first implementaion of web server

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"webservice/handlers"
+)
+
 func main() {
 
+	http.HandleFunc("/", handlers.Router)
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
