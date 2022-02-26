3892786b3d5dcda9949fdf112836f8f00e4192bf
Shai Hod <silvering@gmail.com>
2022-02-26 18:31:56 +0200

    added logger

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -8,13 +8,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", handlers.Router)
+
 	log.Println("Listening...")
+
 	err := http.ListenAndServe(":3000", mux)
-
-	// http.HandleFunc("/", handlers.Router)
-
-	// err := http.ListenAndServe(":3000", nil)
 
 	if err != nil {
 		log.Fatal(err)
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,30 +11,31 @@ import (
 var regexExpresation, _ = regexp.Compile("[*]")
 
 func Router(w http.ResponseWriter, r *http.Request) {
+	log.Println("URI: " + r.URL.Path)
 
-	if r.Header.Get("Content-type") == "text/html" {
-		fmt.Println(r.URL.Path)
-
-		switch {
-		case regexExpresation.MatchString(r.URL.Path):
-			defaultHandler(w, r)
-		default:
-			defaultHandler(w, r)
-		}
+	switch {
+	case regexExpresation.MatchString(r.URL.Path):
+		defaultHandler(w, r)
+	default:
+		defaultHandler(w, r)
 	}
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-type") == "text/html" {
+		log.Println("Request handled by default handler")
+		content, err := io.ReadAll(r.Body)
 
-	fmt.Println("Request handled by default handler")
-	content, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Request body cannot be read")
+		}
 
-	if err != nil {
-		log.Println("Request body cannot be read")
+		log.Println("Request body: " + string(content))
+
+		tm := time.Now().Format(time.RFC1123)
+		w.Write([]byte("The time is: " + tm))
+
+	} else {
+		log.Println("Received a request with a different content type. Type: " + r.Header.Get("Content-type"))
 	}
-
-	log.Println(string(content))
-
-	tm := time.Now().Format(time.RFC1123)
-	w.Write([]byte("The time is: " + tm))
 }
